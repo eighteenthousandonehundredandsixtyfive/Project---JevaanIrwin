be30bb9af27c7f60c4b71d1befb2653c3b0f17f9
Jevaan Irwin <18145@students.mrgs.school.nz>
2021-05-04 13:31:45 +1200

    Update TelemetryProject.py

--- TelemetryProject.py
+++ TelemetryProject.py
@@ -1,1 +1,24 @@
 #Telemtry process
+#Use pyinstaller to package as exe, must be done on hime pc
+#%%
+from tkinter import * #imports everything form tkinter
+import time  #imports time functality
+
+class main_menu():
+    def __init__(self, parent):
+        intro_text = Label(
+            parent,
+            text = "MRGS Telemetry Monitor",
+            fg = "Black",
+            bg = "white"
+        )
+
+        intro_text.place(#...)
+
+        print ("hello world")
+
+root = Tk()
+root.geometry("400x250+300+300")
+main_menu(root)
+root.mainloop()
+# %%
