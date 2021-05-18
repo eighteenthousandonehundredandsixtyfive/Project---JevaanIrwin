b25c5097a39f8f3eca50ee89fb25ac9b8f093d27
Jevaan Iriwn <18145@students.mrgs.school.nz>
2021-05-19 09:14:37 +1200

    Update TelemetryProject.py

--- TelemetryProject.py
+++ TelemetryProject.py
@@ -8,20 +8,21 @@ class Main_menu():
     def __init__(self, parent):
         print("Intro_text start")
 
-        global screen_ratio_h 
-        global screen_ratio_w 
-        screen_ratio_h = screen_height/1080 # determines the ratio between the screen dimensiosn that the program is designed for and the demensions of the screen 
-        screen_ratio_w = screen_width/1920  # in use, so that the ui elements can be scaled appropriatly
-        print(screen_ratio_h)
-        print(screen_ratio_w)
+        global rh #rh being the ratio of the computer resoulotion to 1080p, and rw is the widths ratio
+        global rw 
+        rh = 1080/screen_height # determines the ratio between the screen dimensiosn that the program is designed for and the demensions of the screen 
+        rw = 1920/screen_width  # in use, so that the ui elements can be scaled appropriatly
+        print(rh)
+        print(rw)
 
         intro_text = Label(parent, text = "MRGS Telemetry Monitor", fg = "Black", bg = "white", width = "20")
-        intro_text.place(y = 10 * screen_ratio_h, x = 10 * screen_ratio_w)
+        intro_text.place(y = 10 * rh, x = 25 * rw)
 
         telemetry_button = Button (parent, text = "Telemetry processing", width = 25, height = 1, bg = "white", fg = "black")
-        Raw_data_button = Button (parent, text = "Raw Data Processing", width = 25, height = 1, bg = "white", fg = "black")
+        raw_data_button = Button (parent, text = "Raw Data Processing", width = 25, height = 1, bg = "white", fg = "black")
 
-        telemetry_button.place(y= 20 * screen_ratio_h)
+        telemetry_button.place(x = 10 * rw, y= 30 * rh)
+        raw_data_button.place(x = 200 * rw, y = 30 * rh)
        
 
 main_window = Tk()
