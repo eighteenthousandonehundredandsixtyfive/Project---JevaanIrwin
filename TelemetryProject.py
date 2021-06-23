cb01f47443915ae9903337cf1c0862afb8601fce
Jevaan Irwin <18145@students.mrgs.school.nz>
2021-06-23 21:17:58 +1200

    Update TelemetryProject.py

--- TelemetryProject.py
+++ TelemetryProject.py
@@ -15,64 +15,87 @@ class Flight_sim():
         Sim_window = Tk()
         Sim_window.title('Flight Simulation')
         Sim_window.geometry(f'{screen_width}x{screen_height}')
+        Sim_window.configure(bg = "white")
 
         def Simulation(): 
             thrust = Thrust_text.get()
             weight = Weight_text.get()
             r_time = Time_text.get()
-            print ("simulating")
-            i = float(thrust)  # this is impulse # all the entry boxes must be filled in or else error will be returned. Need to add featrues sucha seasier navigation with tab, pressing enter etc. 
-            w = float(weight)
-            t = float(r_time)
-  
-            T = i / t #T = thrust
-            m = w / 9.81
-            a = (T - w) / m
-            d_b = (0.5) * (a) * (t ** 2) # distance during the burn time . 
-            v = a * t # v being the time after burn
-            E = (0.5) * (m) * (v ** 2)
-            d_c = E / (m * 9.81) # d_c being the distance coasted to apoapsis
-            print (d_b, d_c, d_b + d_c) 
-            d_m = d_c + d_b # maximum height theoretically.
 
-            date = datetime.datetime.now()
-            Date_no = date.strftime("%j")
-            print(Date_no)
-            Date_no = str(Date_no) #limtis the data to being updated once a day. This system will do for now, however needs to be replaced with a version that allows for more user control over what is bieng loaded, and what is being saved. i.e. being able to delete shit. 
-            Burn_distance = "d_b" + Date_no
-            Coast_distance = 'd_c' + Date_no
-            Max_distance = 'd_m' + Date_no   
-            
-            Flight_data = {
-                Burn_distance :  d_b, Coast_distance: d_c, Max_distance: d_m
-            }
+            warned = False
+            File_warn = Label(Sim_window, text = "Cannot be empty, or word", bg = 'white', fg = 'red')
+            File_warn_2 = Label(Sim_window, text = "Cannot be empty", bg = 'white', fg = 'red')
 
-            with open("flight_data.json", "w") as write_file:
-                json.dump(Flight_data, write_file)
+            if len(thrust) == 0 or thrust.isdigit() == False or  len(weight) == 0 or  weight.isdigit() == False or len(r_time) == 0 or  r_time.isdigit() == False: #checks to make sure each field contains values else the program wont work
+                warned = True
+                if len(thrust) == 0 or thrust.isdigit() == False: # it then indiviually checks each entry feild to see where a warning needs to be placed
+                    File_warn.place(x = 210 * rw, y = 40 * rh)
+                elif len(weight) == 0 or  weight.isdigit() == False:
+                    File_warn.place(x = 210 * rw, y = 60 * rh)
+                elif len(r_time) == 0 or  r_time.isdigit() == False:
+                    File_warn.place(x = 210 * rw, y = 80 * rh)
+            else:
+                if warned == True: #not working as intended though not vital to functionality
+                    File_warn.delete()
+                    warned = False
+
+                print ("simulating")
+                i = float(thrust)  # this is impulse # all the entry boxes must be filled in or else error will be returned. Need to add featrues sucha seasier navigation with tab, pressing enter etc. 
+                w = float(weight)
+                t = float(r_time)
+    
+                T = i / t #T = thrust
+                m = w / 9.81
+                a = (T - w) / m
+                d_b = (0.5) * (a) * (t ** 2) # distance during the burn time . 
+                v = a * t # v being the time after burn
+                E = (0.5) * (m) * (v ** 2)
+                d_c = E / (m * 9.81) # d_c being the distance coasted to apoapsis
+                print (d_b, d_c, d_b + d_c) 
+                d_m = d_c + d_b # maximum height theoretically.
+
+                File_name = Data_name.get()
+
+                if len(File_name) == 0: # checks to make sure there is an entry for the data. 
+                    File_warn_2.place(x = 210 * rw, y = 118 * rh)
+                else:
+                    if len(File_name) == 0:
+                        File_warn_2.delete()
+                    
+                    Burn_distance = "d_b"
+                    Coast_distance = 'd_c'
+                    Max_distance = 'd_m'  
+                    
+                    Flight_data = {
+                        Burn_distance :  d_b, Coast_distance: d_c, Max_distance: d_m
+                    }
+
+                    with open("flight_data.json", "w") as write_file:
+                        json.dump(Flight_data, write_file)
 
 
         Thrust_text = Entry(Sim_window, width = 5, bg = 'white', fg = 'black')
         Thrust_label= Label(Sim_window, text = 'Avg. Thrust', width = 10, height = 1, bg = 'white', fg = "black")
-        Thrust_text.place(x = 140 * rw, y = 40 * rh)
+        Thrust_text.place(x = 150 * rw, y = 40 * rh)
         Thrust_label.place(x = 70 * rw, y = 42 * rh)
 
         Weight_text = Entry(Sim_window, width = 5, bg = 'white', fg = "black")
         Weight_label = Label(Sim_window, text = 'Total Weight', width = 10, bg = 'white', fg = 'black')
-        Weight_text.place(x = 140 * rw, y = 60 * rh)
+        Weight_text.place(x = 150 * rw, y = 60 * rh)
         Weight_label.place(x = 70 * rw, y = 62 * rh)
 
         Time_text = Entry(Sim_window, width = 5, bg = 'white', fg = "black")
         Time_label = Label(Sim_window, text = 'Burn Time', width = 10, bg = 'white', fg = 'black')
-        Time_text.place(x = 140 * rw, y = 80 * rh)
+        Time_text.place(x = 150 * rw, y = 80 * rh)
         Time_label.place(x = 70 * rw, y = 82 * rh)
 
         Simulate_button = Button(Sim_window, text = "simulate", width = 30, height= 1, bg = "white", fg = 'black', command = lambda:[Simulation()])
         Simulate_button.place(x = 60 * rw, y = 140 * rh)
 
-        Date_name = Entry(Sim_window, width = 30, bg = "white", fg = 'black')
-        Date_name_label = Label(Sim_window, text = "Data Name", fg = "black", bg = "white", borderwidth= 1, relief = "ridge", width = 29)
-        Date_name.place(x = 58 * rw, y = 118 * rh)
-        Date_name_label.place(x = 60 * rw, y = 100 * rh)
+        Data_name = Entry(Sim_window, width = 30, bg = "white", fg = 'black')
+        Data_name_label = Label(Sim_window, text = "Data Name", fg = "black", bg = "white", width = 29)
+        Data_name.place(x = 58 * rw, y = 118 * rh)
+        Data_name_label.place(x = 60 * rw, y = 100 * rh)
 
 class Raw_data(): #this class contains the code for the raw_data window
     def __init__(self):
